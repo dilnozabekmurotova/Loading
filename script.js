c2c807b6560e971782089407581a0097a8d0ca96
DilnozaKhon <dilnozabekmurotova@gmail.com>
2021-08-11 11:17:32 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+	const loader=document.querySelector('.loader')
+	setTimeout(()=>{
+		loader.style.opacity='0'
+		setTimeout(()=>{
+			loader.style.display='none'
+		},500)
+		
+	},1000)
+})
+
+
